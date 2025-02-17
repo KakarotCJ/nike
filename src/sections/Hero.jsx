9637820c0f012aa71d7b065309b4e229a3b68231
santhosh M <santhoshbca878@gmail.com>
2025-02-17 16:43:47 +0530

    changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import Button from "../components/Button"
+import Button from "../components/Button"s
 import { useState } from "react";
 import { arrowRight } from "../assets/icons"
 import { shoes, statistics } from "../constants"
@@ -12,8 +12,8 @@ const [bigShoeImg, setbigShoeImg] = useState(bigShoe1)
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container pl-10">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
       <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
-      <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
-       <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10"> The New Arrival</span>
+      <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-normel font-bold">
+       <span className="xl:bg-white xl:white space-nowrap relative z-10 pr-10"> The New Arrival</span>
        <br />
        <span className="text-coral-red inline-block mt-3" >Nike  </span> 
         Shoes      
